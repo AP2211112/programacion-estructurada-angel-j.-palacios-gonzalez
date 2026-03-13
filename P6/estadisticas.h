0520a4cbf0b40fd1d5b8c73009a0ad0f6e59b27a
angel j. palacios g. <palacios.angel@uabc.edu.mx>
2026-03-13 23:11:47 +0000

    estadistica.h actua;l

--- P6/estadisticas.h
+++ P6/estadisticas.h
@@ -2,4 +2,4 @@ float calcularpromedio(float calificaciones[], int n);
 float obtenermayor(float calificaciones[], int n);
 float obtenermenor(float [], int );
 int contaraprobados(float calificaciones[],int n);
-int contarreprobados(float calificaciones[], int n);
+int contarreprobados(float calificaciones[], int n);gghf
