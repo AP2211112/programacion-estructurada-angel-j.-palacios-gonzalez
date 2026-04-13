6c8696c64bb9f4030afdbadf35f9f3eb0b16afc6
angel j. palacios g. <palacios.angel@uabc.edu.mx>
2026-04-13 22:39:36 +0000

    k

---
+++ .vscode/imprimir.c/imprimir .c
@@ -0,0 +1,17 @@
+typdetruct{
+    int x
+    int y
+}*matris 
+
+
+
+
+void print int matri(int*m ,int ren, int col){
+    int i,j;  
+    for (i=0; i<ren;i++){
+        for (i,j; j<col; j++){
+            print f{"%d", m[i][j]);}
+        };
+    };
+
+};
--- lab.9/fibonacci.c
+++ lab.9/fibonacci.c
@@ -2,10 +2,15 @@
 //matricula 2211112
 
 #include<stdio.h>
+
+long fibonacci(int n);
+
 main(){
-n=5;
+ int n=5;
 //definicion de la funcion recursiva
-long fn(int n){
+long int fibonacci(int n); {
+
+    int n=5, fn=0;
 
 //1. caso base:definir la recuersion cuando nes 0 o 1
 if (n <= 1){
@@ -13,7 +18,7 @@ if (n <= 1){
 }
 //2. paso recuersivo: la funcion se llama a si misma con (n-1)
 else{
-    return fn=(n-1+fn(n-2));
-};
-}printf("factrotial es %ld", factorial(n));
+    return fn=(n-1+fibonacci(n-2));
+}
+}
 }
