d265ff6a4958638913e804b2418460e832ba2a80
angel j. palacios g. <palacios.angel@uabc.edu.mx>
2026-04-17 11:40:15 -0700

    Update s.c to include the new implementation

--- lab.9/s.c
+++ lab.9/s.c
@@ -8,18 +8,18 @@ int suma(int n);
 int main()
 {
 	int n=5;
-printf(suma es %d\n", suma(n));
-return 0;
+	printf("suma es %d\n", suma(n));
+	return 0;
 }
 //definicion de la funcion recursiva
 int suma(int n){
 
 //1. caso base:definir la recuersion cuando nes 0 o 1
-if (n <= 1){
-    return 1;
-}
+	if (n <= 1){
+	    return 1;
+	}
 //2. paso recuersivo: la funcion se llama a si misma con (n-1)
-else{
-    return n+suma(n-1);
-}
+	else{
+	    return n+suma(n-1);
+	}
 }
